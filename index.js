8d5135760eafcf2b50c66b88a7d9608b53d5fe16
Binh05 <binhdt0452@gmail.com>
2025-06-25 11:33:12 +0700

    initial push

---
+++ index.js
@@ -0,0 +1,43 @@
+const timeFrames = document.querySelectorAll('ul li')
+let currentTimeFrame = document.querySelector('ul li:nth-child(2)')
+const items = document.querySelectorAll('.item')
+
+function changeData(data, timeFrame, time) {
+    items.forEach((item, index) => {
+        const current = item.querySelector('.hours')
+        const previous = item.querySelector('.time')
+
+        if (current && previous) {
+            current.textContent = `${data[index].timeframes[timeFrame].current}hrs`;
+            previous.textContent = `${time} - ${data[index].timeframes[timeFrame].previous}hrs`;
+        }
+    });
+}
+
+timeFrames.forEach((timeFrame) => {
+    timeFrame.addEventListener('click', (e) => {
+
+        currentTimeFrame.classList.remove('active');
+
+        e.target.classList.add('active')
+        currentTimeFrame = e.target
+        const timeFrameName = e.target.textContent.toLowerCase()
+
+        fetch('data.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let previous
+            switch (timeFrameName) {
+                case 'daily':
+                    previous = 'Yesterday'
+                    break;
+                case 'weekly':
+                    previous = 'Last Week'
+                    break;
+                case 'monthly':
+                    previous = 'Last Month'
+            }
+            changeData(data, timeFrameName, previous)
+        })
+    })
+})
